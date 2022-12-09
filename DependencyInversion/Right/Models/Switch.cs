fa0cf0c35034e4edecb2fc199d15435ca2e7eb76
Luiz Parente <lp.parente@gmail.com>
2022-12-09 00:27:37 -0500

    Adding comments.

--- DependencyInversion/Right/Models/Switch.cs
+++ DependencyInversion/Right/Models/Switch.cs
@@ -10,6 +10,12 @@
 			this._light.IsOn = !this._light.IsOn;
 		}
 
+		// A custom type should always be declared in its own file.
+		// In this case, ILightBulb is declared here on purpose,
+		// to convey the idea that the Switch owns the responsibility
+		// of defining the specs of the things on which it has a dependency.
+		// For example, if a service has a dependency on a respository, then
+		// the service must define that repository's specs, usually, via an interface. 
 		public interface ILightBulb {
 			public bool IsOn { get; set; }
 		}
